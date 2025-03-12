39b31651a19d3af8bbfb04906900ccbe7c68ccfa
foxgametothemoon <foxgametothemoon@gmail.com>
2025-03-12 02:08:01 -0400

    Updated video and hero section

--- src/components/GamePreview/GamePreview.jsx
+++ src/components/GamePreview/GamePreview.jsx
@@ -7,10 +7,11 @@ const GamePreview = ({ fullSize = false }) => {
   return (
     <div className={previewClass}>
       {/* Placeholder for game preview - replace with actual video or GIF embed or image */}
-      <img
+      {/* <img
         src="/images/game-preview-placeholder.gif" // Or a static image like game-preview-placeholder.png
         alt="Game Preview"
-      />
+      /> */}
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/PHskP9wglJ8?si=7UqFETVY1W5p3kY3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   );
 };
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./HeroSection.module.css";
 import Button from "../Button/Button"; // Assuming Button component is in the same components folder
+import disha from "../../assets/disha-patani.jpg"
 
 const HeroSection = ({ title, subtitle }) => {
   return (
@@ -17,7 +18,7 @@ const HeroSection = ({ title, subtitle }) => {
       </div>
       <div className={styles.heroImage}>
         {/* Placeholder image - replace with your actual hero image or video */}
-        <img src="/images/hero-placeholder.png" alt="Game Hero" />
+        <img src={disha} alt="Game Hero" />
       </div>
     </section>
   );
